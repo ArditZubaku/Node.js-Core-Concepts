7517616efd242814f44f10b114feefd57e2689ce
ArditZubaku <zubaku92@gmail.com>
2024-07-21 04:06:15 +0200

    Introduction

---
+++ Introduction/app.mjs
@@ -0,0 +1,21 @@
+import { createServer } from "node:http";
+import { readFileSync } from "node:fs";
+import { log } from "node:console";
+
+const server = createServer();
+
+server.on("request", (req, res) => {
+  if (req) {
+    log("Incoming request", req.method);
+  }
+  const result = readFileSync("./Test.txt");
+
+  res.setHeader("Content-Type", "text/plain");
+
+  res.end(result);
+})
+
+server.listen(4080, "127.0.0.1", () => {
+  log("Server has started on:", server.address());
+})
+
---
+++ Introduction/cpuInfo.js
@@ -0,0 +1,28 @@
+const { log } = require('console');
+const os = require('os');
+
+// Get an array of objects containing information about each logical CPU core
+const cpus = os.cpus();
+const architecture = os.arch();
+const type = os.type();
+const name = os.hostname();
+
+log(`Architecture: ${architecture}`);
+log(`Type: ${type}`);
+log(`Hostname: ${name}`);
+
+
+
+// Display information for each CPU core
+cpus.forEach((cpu, index) => {
+  log(`CPU ${index + 1}:`);
+  log(`  Model: ${cpu.model}`);
+  log(`  Speed: ${cpu.speed} MHz`);
+  log(`  Times:`);
+  log(`    User: ${cpu.times.user}`);
+  log(`    Nice: ${cpu.times.nice}`);
+  log(`    Sys: ${cpu.times.sys}`);
+  log(`    Idle: ${cpu.times.idle}`);
+  log(`    IRQ: ${cpu.times.irq}`);
+  log();
+});
